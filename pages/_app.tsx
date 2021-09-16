93738437abde185d46b869173a6e6c662ab9efdd
Edqe14 <configxgt@gmail.com>
2021-09-16 14:39:35 +0700

    add assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Museum</title>
+
+        <link
+          rel='preload'
+          href='/assets/fonts/minecraft-enchantment.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='true'
+        />
+
+        <link
+          rel='preload'
+          href='/assets/fonts/Minecraftia-Regular.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='true'
+        />
+
+        <link
+          rel='preload'
+          href='/assets/fonts/MinecraftTen.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='true'
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default App;
