554476cc3175240d55dabcbbc52339e50c43b232
Edqe14 <configxgt@gmail.com>
2021-09-27 22:43:25 +0700

    add links to header/navbar

--- components/homepage/Header.tsx
+++ components/homepage/Header.tsx
@@ -1,24 +1,35 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className='fixed w-full px-24 py-10 z-20'>
       <nav className='navbar flex justify-between items-center'>
         <div className='nav-right'>
-          <button className='mv-btn btn-blue flex items-center'>
-            <FontAwesomeIcon
-              icon={faEye}
-              className='w-3 cursor-pointer filter drop-shadow-lg'
-            />
-            <span className='font-minecraftia pl-3'>3D Viewer</span>
-          </button>
+          <Link href='/maps' passHref>
+            <button className='mv-btn btn-blue flex items-center'>
+              <FontAwesomeIcon
+                icon={faEye}
+                className='w-3 cursor-pointer filter drop-shadow-lg'
+              />
+              <span className='font-minecraftia pl-3'>3D Viewer</span>
+            </button>
+          </Link>
         </div>
         <div className='nav-left flex flex-row items-center'>
-          <a className='nav-item'>About Project</a>
-          <a className='nav-item'>3D Viewer</a>
-          <a className='nav-item'>Map Makers</a>
-          <a className='nav-item'>Tutorials</a>
+          <a className='nav-item cursor-pointer' href='#'>
+            Home
+          </a>
+          <a className='nav-item cursor-pointer' href='#about-museum'>
+            About Project
+          </a>
+          <a className='nav-item cursor-pointer' href='#team'>
+            Map Makers
+          </a>
+          <a className='nav-item cursor-pointer' href='#tutorial'>
+            Tutorials
+          </a>
         </div>
       </nav>
     </div>
